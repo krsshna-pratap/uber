48725996f0838d4b8ced05b426c03864fc80af02
krsshna <krsshnayadav@gmail.com>
2025-07-27 23:55:05 +0530

    nothing important here, just a commit message

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,10 +4,9 @@ const { body } = require("express-validator");
 const userController = require('../controllers/user.controller');
 
 router.post('/register', [
+  body('email').isEmail().withMessage('invalid email'),
   body('fullName.firstName').isLength({ min: 3 }).withMessage('first name must be at least 3 characters long'),
   body('fullName.lastName').isLength({ min: 3 }).withMessage('last name must be at least 3 characters long'),
-  body('email').isEmail().withMessage('invalid email'),
-  body('password').isLength({ min: 6 }).withMessage('password must be at least 6 characters long'),
-], userController.registerUser);
+], userController.registerUser)
 
 module.exports = router;
